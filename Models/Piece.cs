c01fa543cf37c3444c93fccb997bb5c91b2ae8b7
jxm8022 <jose.mendoza5@mavs.uta.edu>
2022-04-21 09:36:35 -0500

    Created piece module

---
+++ Models/Piece.cs
@@ -0,0 +1,10 @@
+namespace Model;
+
+public class Piece
+{
+    public int PieceID { get; set; } = -1;
+    public int BoardID { get; set; } = -1;
+    public int PlayerID { get; set; } = -1;
+    public int xCoordinate { get; set; } = -1;
+    public int yCoordinate { get; set; } = -1;
+}
