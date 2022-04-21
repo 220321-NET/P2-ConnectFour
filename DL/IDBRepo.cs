74a30314e000f0cbdf99099b84a8b2245d88a875
Donte Jean-Pierre <dontejp@gmail.com>
2022-04-21 16:23:58 -0400

    Restructured IDBRepo and Assigned tasks

--- DL/IDBRepo.cs
+++ DL/IDBRepo.cs
@@ -4,36 +4,64 @@ namespace DL;
 
 public interface IDBRepo
 {
-    //Leaderbords
+
+
+//_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
+//          Family Member: Bailey
+//_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
+
+    //Ranking system
+    public void UpdateRank(Board board);
+    
+//_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
+
+
+
+
+
+//_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
+//          Family Member: Gianny
+//_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
+
+    //Leaderboards
     public List<Player> GetAllPlayers();
-
-
 
     //Handles signup
     public Player CreatePlayer(Player playerToCreate);
 
-
     //Searching player profile before game 
     public Player GetPlayer(string username);
 
+//_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
 
+
+
+
+
+//_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
+//          Family Member:  Jose
+//_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
 
     //Piece information
     public Piece GetPiece();
     public Piece CreatePiece();
 
+//_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
 
+
+
+
+
+//_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
+//          Family Member:  Donte
+//_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
 
     //Board
     public Board GetBoard();
 
-
-
-    //Ranking system
-    public void UpdateRank(Board board);
-
-
-
     //Create a lobby
     public int CreateLobby();
+
+//_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
+
 }
