432223abcd35c692a902697582db6ed132f6920d
jxm8022 <jose.mendoza5@mavs.uta.edu>
2022-04-21 09:36:24 -0500

    Stores pieces instead of list of ints

--- Models/Board.cs
+++ Models/Board.cs
@@ -5,5 +5,5 @@ public class Board
     public int BoardID { get; set; } = -1;
     public int Player1ID { get; set; } = -1;
     public int Player2ID { get; set; } = -1;
-    public List<List<int>> BoardState { get; set; } = List<List<int>>();
+    public List<Piece> BoardState { get; set; } = List<Piece>();
 }
