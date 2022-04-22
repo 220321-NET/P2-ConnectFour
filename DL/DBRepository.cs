089ecd7e8035451ee761738fab95a09790c442e1
jxm8022 <jose.mendoza5@mavs.uta.edu>
2022-04-22 11:40:24 -0500

    Added methods for interface

--- DL/DBRepository.cs
+++ DL/DBRepository.cs
@@ -3,20 +3,87 @@
 namespace DL;
 
 
-public class DBRepository: IDBRepo
+public class DBRepository : IDBRepo
 {
+    //_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
+    //          Family Member: Bailey
+    //_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
 
+    //Ranking system
+    public void UpdateRank(Board board)
+    {
+
+    }
+
+    //_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
+
+
+
+
+
+    //_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
+    //          Family Member: Gianny
+    //_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
+
+    //Leaderboards
+    public List<Player> GetAllPlayers()
+    {
+        return new List<Player>();
+    }
+
+    //Handles signup
+    public Player CreatePlayer(Player playerToCreate)
+    {
+        return new Player();
+    }
+
+    //Searching player profile before game 
+    public Player GetPlayer(string username)
+    {
+        return new Player();
+    }
+
+    //_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
+
+
+
+
+
+    //_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
+    //          Family Member:  Jose
+    //_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
+
+    //Piece information
+    public Piece GetPiece()
+    {
+        return new Piece();
+    }
+    public Piece CreatePiece()
+    {
+        return new Piece();
+    }
+
+    //_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
+
+
+
+
+
+    //_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
+    //          Family Member:  Donte
+    //_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
+
+    //Board
+    public Board GetBoard()
+    {
+        return new Board();
+    }
+
+    //Create a lobby
     public int CreateLobby()
     {
-        //Grab GameID
-
-        //Create Board
+        return -1;
     }
 
-    public void UpdateRank(Board board)
-    { 
-        //Grab WinID and give win to player
-
-        //In the case of -1 incriment both players
-    }
+    //_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_/~\_
 }
