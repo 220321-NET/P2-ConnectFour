5f79a6ce81185210c2aa11ffb7e16af6057f712d
jxm8022 <jose.mendoza5@mavs.uta.edu>
2022-04-22 11:39:50 -0500

    Updated program.cs

--- Connect4API/Program.cs
+++ Connect4API/Program.cs
@@ -1,11 +1,22 @@
+using DL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Host.UseSerilog(
+    (ctx, lc) => lc
+    .WriteTo.Console()
+    .WriteTo.File("../logs/Connect4Logs.txt", rollingInterval: RollingInterval.Day)
+);
+
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DBContext>(options => options.UseNpgSql(builder.Configuration.GetConnectionString("PostgreDB")));
 
 var app = builder.Build();
 
