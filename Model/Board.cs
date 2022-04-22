e80f26fffb3c71ceb5202fe0aeb3f94b0030fc9f
jxm8022 <jose.mendoza5@mavs.uta.edu>
2022-04-22 11:40:46 -0500

    Forgot new keyword

--- Model/Board.cs
+++ Model/Board.cs
@@ -5,6 +5,6 @@ public class Board
     public int BoardID { get; set; } = -1;
     public int Player1ID { get; set; } = -1;
     public int Player2ID { get; set; } = -1;
-    public int WinID{get; set;} = -1;
-    public List<Piece> BoardState { get; set; } = List<Piece>();
+    public int WinID { get; set; } = -1;
+    public List<Piece> BoardState { get; set; } = new List<Piece>();
 }
