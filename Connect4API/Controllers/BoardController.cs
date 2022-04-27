c1d328fd813d478ed6e357ee09b716d4733d3037
jxm8022 <jose.mendoza5@mavs.uta.edu>
2022-04-27 16:58:25 -0500

    Fixed board and lobby

--- Connect4API/Controllers/BoardController.cs
+++ Connect4API/Controllers/BoardController.cs
@@ -15,17 +15,10 @@ namespace Connect4API.Controllers
         private readonly IDBRepo _dl;
 
         public BoardController(IDBRepo dl, ILogger<BoardController> logger)
-        { 
+        {
             _dl = dl;
             _logger = logger;
         }
-
-        // GET: api/<ValuesController>
-        //[HttpGet]
-        //public IEnumerable<string> Get()
-        //{
-        //    return new string[] { "value1", "value2" };
-        //}
 
         // GET api/<ValuesController>/5
         [HttpGet("{LobbyId}")]
@@ -40,17 +33,5 @@ namespace Connect4API.Controllers
         {
             return _dl.CreateBoard(board);
         }
-
-        //// PUT api/<ValuesController>/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody] string value)
-        //{
-        //}
-
-        //DELETE api/<ValuesController>/5
-        //[HttpDelete("{id}")]
-        //public void Delete(int id)
-        //{
-        //}
     }
 }
